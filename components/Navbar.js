c73caa8e5bd3e3bac4333b6ea6a3f6f326fecca9
Ashley Hart <ahart425@gmail.com>
2023-01-29 20:19:09 -0700

    adding style and color to homepage

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+    return (
+        <>
+        <nav className="navbar">
+            <div className="container-fluid navbarStyle d-flex">
+                <Link href='/'>Home</Link>
+                <Link href='/about'>About</Link>
+                <Link href='/projects'>Projects</Link>
+            </div>
+        </nav>
+            
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
 import Head from 'next/head'
-import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Ashley Hart Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+          <div className="row">
+            <div className="col-xs-12 col-sm-8 text-center">
+              <p className='light-text'>I'm <span className='dark-text'>Ashley Hart</span></p> 
+              <p className='light-text'>Front End <span className='dark-text'>Web Developer</span></p>
+              <p className='light-text'>I am a <span className='dark-text'>customer service guru</span> turned <span className='dark-text'>programmer</span> I enjoy learning new technologies, putting my skills to the test, and being a part of making the world around me a better place</p>
+              <p className='light-text'>While not online, I enjoy <span className='dark-text'>running trails</span> in the high country, friendly competition at the <span className='dark-text'>CrossFit</span> gym, and <span className='dark-text'>camping</span> under the stars in the summer</p>
+            </div>
+              <div className="col-xs-12 col-sm-4 text-center">
+                <Image 
+                src="/images/profile.jpg"
+                height={350}
+                width={350}
+                alt="author holding a cup of tea"
+                className='profile-photo'
+                />
+              </div>
+          </div>
     </div>
   )
 }
